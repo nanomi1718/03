53741b6cc44149f25474f50bb7f93cc22d2bfcd2
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-07-16 05:17:32 +0900

    실습3

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+	int ja, mo;
+	printf("enter bunja : ");
+	scanf("%d", &ja);
+	
+	printf("enter bunmo : ");
+	scanf("%d", &mo);
+	
+	printf("the result is %f\n", (float)ja/mo);
+	
+	return 0;
+	
+}
