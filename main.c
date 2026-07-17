429faa85b09bf981db85cb2281fe3c96b2a64674
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-07-18 02:04:01 +0900

    실습3주차-1

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
-{
-	int ja, mo;
-	printf("enter bunja : ");
-	scanf("%d", &ja);
+int main(void){
+	int input_int;
+	float input_float; 
 	
-	printf("enter bunmo : ");
-	scanf("%d", &mo);
+	printf("enter an intenger : ");
+	scanf("%d", &input_int);
 	
-	printf("the result is %f\n", (float)ja/mo);
+	printf("enter a float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
 	
 	return 0;
-	
 }
