5b46feebf4382f9fbce14d638d209beb17c74614
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-09-17 16:51:27 +0900

    03-3

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main(void){
-	char c;
-	int i;
+	char c, c1;
 	
-	printf("input a number : ");
+	printf("enter a character : ");
 	scanf("%c", &c);
 	
-	i= c-'0';
-	printf("The input number is %i\n", i);
+	
+	c1= c + 1;
+	printf("The next character of %c (%d) is %c (%d)\n",c, c, c1, c1);
 	
 	return 0;
 
